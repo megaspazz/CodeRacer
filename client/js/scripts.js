57b39c80cdb744f8138051931b5fd2226a48c8bd
James Hu <james6724898@gmail.com>
2016-11-25 13:51:17 -0500

    initial client commit

---
+++ client/js/scripts.js
@@ -0,0 +1,17 @@
+var lines;
+var counter;
+
+function l(text) {
+	lines = text.split("\n");
+}
+
+function test() {
+	$(document).ready
+}
+
+function check(input) {
+	if(input.length > lines[counter])
+	{
+		
+	}
+}
