e0f71ddff49a38037b9b54ced01fa2fde89fac53
Eric Pan <tom.ed.awe.67@gmail.com>
2016-11-25 23:53:18 -0500

    it is 'working' now

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,3 +1,96 @@
+
+var socket = io();
+
+const States = {
+	NONE: 0,
+	WAITING_FOR_RACE: 1,
+	IN_RACE: 2
+};
+
+var currentState = States.NONE;
+
+var currentRaceID = null;
+var currentRaceText = null;
+var currentRaceStartTimeMS = null;
+
+var currentUserID = ~~(Math.random() * 1000000);    // initialize as the current user's ID
+console.log("user id = " + currentUserID);
+
+var myProgress = {
+	currentLine: null,
+	totalLines: null
+};
+
+function reportProgress() {
+	//console.log("reporting progress");
+	if (currentState === States.IN_RACE) {
+		console.log("    in race!");
+		myProgress.currentLine++;    // set actual progress
+		socket.emit("progress_report", currentRaceID, currentUserID, myProgress);
+	}
+	setTimeout(reportProgress, 1000);
+}
+
+// always check for progress
+reportProgress();
+
+function showCountdown() {
+	var currTime = new Date();
+	var remainingMS = currentRaceStartTimeMS - currTime.getTime();
+	if (remainingMS <= 0) {
+		$("#countdown").text("");
+		currentState = States.IN_RACE;
+	} else {
+		$("#countdown").text(remainingMS);
+		setTimeout(showCountdown, 100);
+	}
+}
+
+socket.on("found_race", function(raceID, raceText) {
+	console.log("got request to start race from server");
+	currentState = States.WAITING_FOR_RACE;
+	currentRaceText = raceText;
+	currentRaceID = raceID;
+});
+
+socket.on("start_race_timer", function(startTime) {
+	console.log("starting race timer");
+	currentRaceStartTimeMS = Date.parse(startTime);
+	showCountdown();
+});
+
+socket.on("race_state", function(raceID, userProgressesJSON) {
+	console.log("got race state");
+	var userProgresses = JSON.parse(userProgressesJSON);
+	for (var id in userProgresses) {
+		var progress = userProgresses[id];
+		if (progress) {
+			// update UI with other racers' progress
+			console.log(id + ": " + progress.currentLine + " / " + progress.totalLines);
+		}
+	}
+});
+
+$("#btnRequestRace").click(function() {
+	socket.emit("race_request", currentUserID);
+});
+
+
+
+
+$("#btnTestServer").click(function() {
+	socket.emit("test request");
+});
+
+socket.on("test_receive", function(txt) {
+	console.log("got request from server");
+	$("#txtCode").text(txt);
+});
+
+
+
+
+
 var lines;
 var counter;
 
---
+++ server/server.js
@@ -0,0 +1,90 @@
+var path = require("path");
+
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+const PORT = 1997;
+
+const CLIENT_PATH = path.resolve(__dirname + "/../client");
+
+var activeRaces = { };
+
+app.use("/css",  express.static(CLIENT_PATH + "/css"));
+app.use("/js",  express.static(CLIENT_PATH + "/js"));
+app.use("/assets",  express.static(CLIENT_PATH + "/assets"));
+
+app.get('/', function(req, res) {
+	res.sendFile(CLIENT_PATH + "/index.html");
+});
+
+http.listen(PORT, function() {
+	console.log("starting server on *:" + PORT);
+});
+
+
+
+
+
+
+io.on("connection", function(socket) {
+	console.log("+ CONNECTION");
+
+	socket.on("race_request", function(userID) {
+		console.log("got race request");
+		let raceID = 1997;
+		let raceText = "lorem ipsum\nherp derp";
+		if (!activeRaces[raceID]) {
+			activeRaces[raceID] = {
+				users: { },
+				startTime: null
+			};
+		}		
+		activeRaces[raceID].users[userID] = {
+			connection: socket,
+			progress: null
+		}
+		console.log(Object.keys(activeRaces[raceID].users).length);
+		if (Object.keys(activeRaces[raceID].users).length >= 2) {
+			if (!activeRaces[raceID].startTime) {
+				let time = new Date();
+				time.setSeconds(time.getSeconds() + 5);
+				activeRaces[raceID].startTime = time;
+			}
+			let actualStartTime = activeRaces[raceID].startTime;
+			for (let userID in activeRaces[raceID].users) {
+				let user = activeRaces[raceID].users[userID];
+				user.connection.emit("start_race_timer", actualStartTime);
+			}
+		}
+		//activeRaces[raceID].users[userID] = null;
+		//activeRaces[raceID].started = true;
+		socket.emit("found_race", raceID, raceText);
+	});
+	
+	socket.on("progress_report", function(raceID, userID, progress) {
+		console.log("got progress report");
+		activeRaces[raceID].users[userID].progress = progress;
+		console.log("didn't die yet");
+		let userProgresses = { };
+		for (let userID in activeRaces[raceID].users) {
+			userProgresses[userID] = activeRaces[raceID].users[userID].progress;
+		}
+		let userProgressesJSON = JSON.stringify(userProgresses);
+		socket.emit("race_state", raceID, userProgressesJSON);
+	});
+
+	socket.on("disconnect", function() {
+		console.log("- DISCONNECTION");
+	});
+	
+	
+	
+	
+	
+	socket.on("test request", function() {
+		console.log("  * GOT REQUEST!");
+		socket.emit("test receive", "lorem ipsum herp derp");
+	});
+});
